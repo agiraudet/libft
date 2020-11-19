65135863d4a5e8156eea9cad24a92a9593e03bdd
agiraude <agiraude@bell.home>
2020-11-19 17:35:42 +0100

    test

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,21 +6,11 @@
 /*   By: agiraude <agiraude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 16:40:40 by agiraude          #+#    #+#             */
-/*   Updated: 2020/11/19 17:28:48 by agiraude         ###   ########.fr       */
+/*   Updated: 2020/11/19 17:34:24 by agiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_check_occurence(const char *hay, const char *needle, size_t len)
-{
-	while (*needle)
-	{
-		if (*hay++ != *needle++ || len-- == 0)
-			return (0);
-	}
-	return (1);
-}
 
 char		*ft_strnstr(const char *big, const char *little, size_t len)
 {
