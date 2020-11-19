2bba13cf7a7ba31e0f6e7c2d2fa40d01cdff6d6f
agiraude <agiraude@bell.home>
2020-11-19 16:45:49 +0100

    test

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: agiraude <agiraude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 14:14:21 by agiraude          #+#    #+#             */
-/*   Updated: 2020/11/19 16:14:48 by agiraude         ###   ########.fr       */
+/*   Updated: 2020/11/19 16:45:22 by agiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	const unsigned char	*s;
 
-	if (!dest || !src || !n)
+	d = (unsigned char*)dest;
+	s = (unsigned char*)src;
+	if (!n || d == s)
 		return (dest);
-	d = dest;
-	s = src;
 	while (n--)
 		*d++ = *s++;
 	return (dest);
