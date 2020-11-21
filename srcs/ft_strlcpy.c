3439e1cfd42629a0d259df0d72fdf304f185d54b
agiraude <agiraude@bell.home>
2020-11-21 17:15:11 +0100

    final version

---
+++ srcs/ft_strlcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agiraude <agiraude@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 16:35:19 by agiraude          #+#    #+#             */
+/*   Updated: 2020/11/17 19:01:45 by agiraude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	srclen;
+
+	srclen = ft_strlen(src);
+	if (!size)
+		return (srclen);
+	while (--size && *src)
+		*dst++ = *src++;
+	*dst = '\0';
+	return (srclen);
+}
