efb62b5eebe1fb53f78bf6f9b4289cb2e57e2594
agiraude <agiraude@bell.home>
2020-11-25 21:12:48 +0100

    protection

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: agiraude <agiraude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 13:15:00 by agiraude          #+#    #+#             */
-/*   Updated: 2020/11/25 21:04:48 by agiraude         ###   ########.fr       */
+/*   Updated: 2020/11/25 21:12:29 by agiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,8 @@ static char		*ft_cutstr(char const *s, size_t len)
 static int		ft_free_tab(char ***tab, size_t i)
 {
 	while (i - 1 > 0)
-		free(**tab[i]);
-	free(**tab);
+		free(*tab[i]);
+	free(*tab);
 	return (0);
 }
 
