3dc487640524f3dcd4c28eaf35ef58eff5c2da62
agiraude <agiraude@student.42.fr>
2021-02-15 01:46:26 +0100

    fixed ft_strcmp

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: agiraude <agiraude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 01:08:50 by agiraude          #+#    #+#             */
-/*   Updated: 2021/02/15 01:14:30 by agiraude         ###   ########.fr       */
+/*   Updated: 2021/02/15 01:45:59 by agiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@ int		ft_strcmp(const char *s1, const char *s2)
 {
 	int		i;
 
+	i = 0;
 	while (s1[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
